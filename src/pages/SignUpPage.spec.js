d53891ffa821e0dfbcf8d1fc48300db3742f4908
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-07-02 12:58:08 -0300

    test: implments Interaction test to SignUpPage

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,6 +1,9 @@
 import SignUpPage from './SignUpPage.vue'
 import { render, screen } from '@testing-library/vue'
 import "@testing-library/jest-dom"
+import userEvent from '@testing-library/user-event'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
 
 describe('SignUpPage', () => {
   describe("Layout", () => {
@@ -48,6 +51,47 @@ describe('SignUpPage', () => {
       render(SignUpPage)
       const button = screen.queryByRole('button', { name: 'Sign Up' })
       expect(button).toBeDisabled()
+    })
+  })
+  describe('Interaction', () => {
+    it('enables the button when the password and password resets matchs', async () => {
+      render(SignUpPage)
+      const passwordInput = screen.queryByLabelText('Password')
+      const passwordRepeatInput = screen.queryByLabelText('Password Repeat')
+      await userEvent.type(passwordInput, "P4ssword")
+      await userEvent.type(passwordRepeatInput, "P4ssword")
+      const button = screen.queryByRole('button', { name: 'Sign Up' })
+      expect(button).toBeEnabled()
+    })
+    it('send username, email and password to backend after clicking the button', async () => {
+      let requestBody
+      const server = setupServer(
+        rest.post('/api/1.0/users', (req, res, ctx) => {
+          requestBody = req.body
+          return res(ctx.status(200))
+        })
+      )
+      server.listen()
+
+      render(SignUpPage)
+      const usernameInput = screen.queryByLabelText('Username')
+      const emailInput = screen.queryByLabelText('E-mail')
+      const passwordInput = screen.queryByLabelText('Password')
+      const passwordRepeatInput = screen.queryByLabelText('Password Repeat')
+      await userEvent.type(usernameInput, "user1")
+      await userEvent.type(emailInput, "user1@mail.com")
+      await userEvent.type(passwordInput, "P4ssword")
+      await userEvent.type(passwordRepeatInput, "P4ssword")
+      const button = screen.queryByRole('button', { name: 'Sign Up' })
+
+      await userEvent.click(button)
+      server.close()
+
+      expect(requestBody).toEqual({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
     })
   })
 })
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 9876
+    port: 9876,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080'
+      }
+    }
   }
 })
 
