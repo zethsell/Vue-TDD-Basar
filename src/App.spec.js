341d2be95a618c6afe6d506d11a9a912bd87cffa
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-07-04 23:04:01 -0300

    feat: implements navbar

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/vue'
 import App from './App.vue'
 import i18n from './locales/i18n'
+import userEvent from '@testing-library/user-event'
 
 const setup = (path) => {
   window.history.pushState({}, '', path)
@@ -45,4 +46,29 @@ describe('Routing', () => {
     const page = screen.queryByTestId(pageTestId)
     expect(page).not.toBeInTheDocument()
   })
+
+  it.each`
+    targetPage
+    ${'Home'}
+    ${'Sign Up'}
+    ${'Login'}
+  `('has a link to $targetPage on Navbar', ({ targetPage }) => {
+    setup('/')
+    const link = screen.queryByRole('link', { name: targetPage })
+    expect(link).toBeInTheDocument()
+  })
+
+  it.each`
+  initialPath | clickingTo    | visiblePage
+  ${'/'}      | ${'Sign Up'}  | ${'signup-page'}
+  ${'/'}      | ${'Login'}    | ${'login-page'}
+  ${'/signup'}| ${'Home'}     | ${'home-page'}
+  `('displays  $visiblePage after clicking $clickingTo link',
+    async ({ initialPath, visiblePage, clickingTo }) => {
+      setup(initialPath)
+      const link = screen.queryByRole('link', { name: clickingTo })
+      await userEvent.click(link)
+      const page = screen.queryByTestId(visiblePage)
+      expect(page).toBeInTheDocument()
+    })
 })
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <a @click.prevent="onClickLink" href="/" title="Home">Home</a>
+    <a @click.prevent="onClickLink" href="/signup">{{ $t('signUp') }}</a>
+    <a @click.prevent="onClickLink" href="/login">Login</a>
     <HomePage v-if="path === '/'" />
     <SignUpPage v-else-if="path === '/signup'" />
     <LoginPage v-else-if="path === '/login'" />
@@ -24,11 +27,18 @@ export default {
     LoginPage,
     UserPage,
   },
-  computed: {
-    path() {
-      return window.location.pathname
+  data() {
+    return {
+      path: window.location.pathname
+    }
+  },
+  methods: {
+    onClickLink(event) {
+      this.path = event.target.attributes.href.value
+      window.history.pushState({}, '', this.path)
     }
   }
 }
 </script >
 
+ 
