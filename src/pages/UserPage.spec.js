6b68808aafbafdedc722f13afc42accabc567e4e
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-07-24 14:02:08 -0300

    feat: implements user page

---
+++ src/pages/UserPage.spec.js
@@ -0,0 +1,55 @@
+import UserPage from './UserPage.vue'
+import { render, screen, waitFor } from '@testing-library/vue'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+
+const server = setupServer(
+  rest.get('/api/1.0/users/:id', (req, res, ctx) => {
+    if (req.params.id === '1') {
+      return res(ctx.status(200), ctx.json({
+        id: 1,
+        username: 'user1',
+        email: 'user1@mail.com',
+        image: null
+      }))
+    } else {
+      return res(ctx.status(404), ctx.json({
+        message: 'User not found'
+      }))
+    }
+  })
+)
+
+beforeAll(() => server.listen())
+
+beforeEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
+
+const setup = (id = 1) => {
+  render(UserPage, {
+    global: { mocks: { $route: { params: { id } } } }
+  })
+}
+
+describe('User Page', () => {
+  it('displays username on page when user is found', async () => {
+    setup()
+    await waitFor(() => {
+      expect(screen.queryByText('user1')).toBeInTheDocument()
+    })
+  })
+
+  it('dsplays spinner while the api call is in progress', () => {
+    setup()
+    const spinner = screen.queryByRole('status')
+    expect(spinner).toBeInTheDocument()
+  })
+
+  it('displays error message received drom backend when user not found', async () => {
+    setup(100)
+    await waitFor(() => {
+      expect(screen.queryByText('User not found')).toBeInTheDocument()
+    })
+  })
+})
--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,5 +1,40 @@
 <template>
   <div data-testid="user-page">
-    <h1>User Page</h1>
+    <ProfileCard :user="user" v-if="!pendingApiCall && !failResponse" />
+    <div class="alert alert-secondary text-center" v-if="pendingApiCall">
+      <Spinner size="normal" />
+    </div>
+    <div class="alert alert-danger text-center" v-if="failResponse">
+      {{ failResponse }}
+    </div>
   </div>
 </template>
+<script>
+import { getUserById } from '../api/apiCalls'
+import ProfileCard from '../components/ProfileCard.vue'
+import Spinner from '../components/SpinnerComponent.vue'
+
+export default {
+  name: "UserPage",
+  components: {
+    ProfileCard,
+    Spinner
+  },
+  data() {
+    return {
+      user: {},
+      pendingApiCall: true,
+      failResponse: undefined
+    }
+  },
+  async mounted() {
+    try {
+      const { data } = await getUserById(this.$route.params.id)
+      this.user = data
+    } catch (error) {
+      this.failResponse = error.response.data.message
+    }
+    this.pendingApiCall = false
+  }
+}
+</script>
